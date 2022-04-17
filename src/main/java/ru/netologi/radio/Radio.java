6c7240b6d16941d8d22e92c3fae5c80b09cdc4b3
Jul <yuosalamatova@gmail.com>
2022-04-17 16:51:48 +0700

    programm and test

--- src/main/java/ru/netologi/radio/Radio.java
+++ src/main/java/ru/netologi/radio/Radio.java
@@ -1,5 +1,66 @@
 package ru.netologi.radio;
 
 public class Radio {
+    private int radioStationNumber;
+    private int increaseVolume;
+
+    public int getRadioStationNumber() {
+        return radioStationNumber;
+    }
+    public int getIncreaseVolume() {
+        return increaseVolume;
+    }
+
+    public void setRadioStationNumber(int radioStationNumber) {
+        if (radioStationNumber < 0) {
+            return;
+        }
+        if (radioStationNumber > 9) {
+            return;
+        }
+        this.radioStationNumber = radioStationNumber;
+    }
+
+    public void setIncreaseVolume(int increaseVolume) {
+        if (increaseVolume < 0) {
+            return;
+        }
+        if (increaseVolume > 10) {
+            return;
+        }
+        this.increaseVolume = increaseVolume;
+    }
+
+    public void setNext(int radioStationNumber) {
+        if (radioStationNumber > 8) {
+            this.radioStationNumber = 0;
+        } else {
+            this.radioStationNumber = radioStationNumber + 1;
+        }
+    }
+
+    public void setPriv(int radioStationNumber) {
+        if (radioStationNumber < 1) {
+            this.radioStationNumber = 9;
+        } else {
+            this.radioStationNumber = radioStationNumber - 1;
+        }
+    }
+    public void setIncreaseVolumeAp(int increaseVolume) {
+        if (increaseVolume > 9) {
+            this.increaseVolume = 10;
+        }else {
+            this.increaseVolume = increaseVolume + 1;
+        }
+    }
+
+    public void setIncreaseVolumeDown(int increaseVolume) {
+        if (increaseVolume < 1) {
+            this.increaseVolume = 0;
+        }else {
+            this.increaseVolume = increaseVolume - 1;
+        }
+    }
+
 
 }
--- src/test/java/ru/netologi/radio/RadioTest.java
+++ src/test/java/ru/netologi/radio/RadioTest.java
@@ -1,7 +1,169 @@
 package ru.netologi.radio;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class RadioTest {
+public class RadioTest {
 
+
+    @Test
+    public void getRadioStationNumberTest() {
+        Radio stat = new Radio();
+     stat.setRadioStationNumber(2);
+
+     int expected = 2;
+     int actual = stat.getRadioStationNumber();
+
+     assertEquals(expected, actual);
+    }
+
+    @Test
+    void setRadioStationNumberTestMax() {
+        Radio stat = new Radio();
+        stat.setRadioStationNumber(11);
+        int expected = 0;
+        int actual = stat.getRadioStationNumber();
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void setRadioStationNumberTestMin() {
+        Radio stat = new Radio();
+        stat.setRadioStationNumber(-2);
+        int expected = 0;
+        int actual = stat.getRadioStationNumber();
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void setRadioStationNumberTestAverage() {
+        Radio stat = new Radio();
+        stat.setRadioStationNumber(5);
+        int expected = 5;
+        int actual = stat.getRadioStationNumber();
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void setNextAverage() {
+        Radio stat = new Radio();
+        stat.setNext(5);
+        int expected = 6;
+        int actual = stat.getRadioStationNumber();
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void setNextMax() {
+        Radio stat = new Radio();
+        stat.setNext(9);
+        int expected = 0;
+        int actual = stat.getRadioStationNumber();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void setPrivMin() {
+        Radio stat = new Radio();
+        stat.setPriv(0);
+        int expected = 9;
+        int actual = stat.getRadioStationNumber();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void setPrivAverage() {
+        Radio stat = new Radio();
+        stat.setPriv(5);
+        int expected = 4;
+        int actual = stat.getRadioStationNumber();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getIncreaseVolume() {
+        Radio stat = new Radio();
+        stat.setIncreaseVolume(2);
+
+        int expected = 2;
+        int actual = stat.getIncreaseVolume();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void setIncreaseVolumeMax() {
+        Radio stat = new Radio();
+        stat.setIncreaseVolume(11);
+
+        int expected = 0;
+        int actual = stat.getIncreaseVolume();
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void setIncreaseVolumeMin() {
+        Radio stat = new Radio();
+        stat.setIncreaseVolume(-1);
+
+        int expected = 0;
+        int actual = stat.getIncreaseVolume();
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void setIncreaseVolumeAverage() {
+        Radio stat = new Radio();
+        stat.setIncreaseVolume(5);
+
+        int expected = 5;
+        int actual = stat.getIncreaseVolume();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void setIncreaseVolumeApMax() {
+        Radio stat = new Radio();
+        stat.setIncreaseVolumeAp(10);
+
+        int expected = 10;
+        int actual = stat.getIncreaseVolume();
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void setIncreaseVolumeApAverage() {
+        Radio stat = new Radio();
+        stat.setIncreaseVolumeAp(5);
+
+        int expected = 6;
+        int actual = stat.getIncreaseVolume();
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void setIncreaseVolumeDownMin() {
+        Radio stat = new Radio();
+        stat.setIncreaseVolumeDown(0);
+
+        int expected = 0;
+        int actual = stat.getIncreaseVolume();
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void setIncreaseVolumeDownAverage() {
+        Radio stat = new Radio();
+        stat.setIncreaseVolumeDown(5);
+
+        int expected = 4;
+        int actual = stat.getIncreaseVolume();
+
+        assertEquals(expected, actual);
+    }
 }
